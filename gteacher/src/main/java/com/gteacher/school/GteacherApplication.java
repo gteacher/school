8bad320a8f6521326717823025ee363b7f9106cd
Mohamed Said EL HMAM <hmam04@yahoo.fr>
2018-09-14 01:03:47 +0200

    test

---
+++ gteacher/src/main/java/com/gteacher/school/GteacherApplication.java
@@ -0,0 +1,13 @@
+package com.gteacher.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GteacherApplication.class, args);
+	}
+	
+}
